c5aa0b0a2725406531a73375504e3f320c4e05aa
oydzhi <bachurinvv54@gmail.com>
2026-05-04 20:58:33 +0500

    ADD: php scripts for send form

---
+++ send_mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["ФИО"];
+    $company = $_POST["Компания"];
+    $email = $_POST["Email"];
+    $phone = $_POST["Телефон"];
+
+    $to = "bachurinvv54@gmail.com";
+    $subject = "Новая заявка с сайта";
+
+    $message = "
+    ФИО: $name
+    Компания: $company
+    Email: $email
+    Телефон: $phone
+    ";
+
+    $headers = "From: no-reply@yourdomain.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    mail($to, $subject, $message, $headers);
+
+    header("Location: thank-you.html");
+    echo "Форма отправлена";
+    print_r($_POST);
+    exit();
+}
+?>
