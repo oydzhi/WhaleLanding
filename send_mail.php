0cf84ffcd22a63eb28f57962bba9ec6c77552404
oydzhi <bachurinvv54@gmail.com>
2026-05-06 19:42:51 +0500

    UPD: email sending script

--- send_mail.php
+++ send_mail.php
@@ -1,28 +1,24 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["ФИО"];
-    $company = $_POST["Компания"];
-    $email = $_POST["Email"];
-    $phone = $_POST["Телефон"];
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    $to = "whale-coms@yandex.ru";
-    $subject = "Новая заявка с сайта";
+$to = "whale-coms@yandex.ru";
+$subject = "Новая заявка с сайта";
 
-    $message = "
-    ФИО: $name
-    Компания: $company
-    Email: $email
-    Телефон: $phone
-    ";
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$position = isset($_POST['position']) ? $_POST['position'] : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$phone = isset($_POST['phone']) ? $_POST['phone'] : '';
 
-    $headers = "From: no-reply@yourdomain.com\r\n";
-    $headers .= "Reply-To: $email\r\n";
+$message = "ФИО: $name\nДолжность: $position\nEmail: $email\nТелефон: $phone";
 
-    mail($to, $subject, $message, $headers);
+$headers = "From: no-reply@softwhale.ru\r\n";
+$headers .= "Reply-To: $email\r\n";
 
-    header("Location: index.html");
-    echo "Форма отправлена";
-    print_r($_POST);
+if (mail($to, $subject, $message, $headers)) {
+    header("Location: /?success=1");
     exit();
+} else {
+    echo "Ошибка отправки";
 }
 ?>
